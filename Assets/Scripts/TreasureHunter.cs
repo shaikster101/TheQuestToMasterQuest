6bd879126dea1f2b9ce58753f55b3697ae306ede
ramvadla8 <vadlamudi.sriram@gmail.com>
2020-02-25 23:22:31 -0500

    VR Treasure Hunt

--- Assets/Scripts/TreasureHunter.cs
+++ Assets/Scripts/TreasureHunter.cs
@@ -18,6 +18,13 @@ public class TreasureHunter : MonoBehaviour
 
     private int score = 0; 
 
+    private int blueNum = 0; 
+    private int greenNum = 0; 
+
+    private int redNum = 0; 
+
+    private int yellowNum = 0; 
+
     GameObject BlueMagic;
     GameObject GreenMagic;
     GameObject RedMagic;
@@ -44,7 +51,7 @@ public class TreasureHunter : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {   
 
       if(rightChecker != null || leftChecker != null){
@@ -57,11 +64,16 @@ public class TreasureHunter : MonoBehaviour
           }
       }
 
+
       if(grabbedObject != null){
 
-            float dist = Vector3.Distance(new Vector3(cameraPos.transform.position.x, cameraPos.transform.position.y - 0.6f, cameraPos.transform.position.z), grabbedObject.transform.position); 
+
+            Vector3 waistPos = new Vector3(cameraPos.transform.position.x, cameraPos.transform.position.y - 0.4f, cameraPos.transform.position.z); 
+            float dist = Vector3.Distance(grabbedObject.transform.position,waistPos); 
             //updateText.text = "" + dist; 
-            if(dist < 0.20f){
+
+            //updateText.text = "" + dist; 
+            if(dist < 0.2f){
 
                 Destroy(grabbedObject);
 
@@ -73,22 +85,26 @@ public class TreasureHunter : MonoBehaviour
                 if(grabbedObject.name.Contains("BlueMagic")){
                         GameInventory.Inventory.Add(BlueMagic);
                         temp = BlueMagic; 
+                        blueNum++; 
                         Debug.Log("Logged Blue"); 
                 }else if(grabbedObject.name.Contains("GreenMagic")){
                         GameInventory.Inventory.Add(GreenMagic);
+                        greenNum++; 
                         temp= GreenMagic; 
                          Debug.Log("Logged Green"); 
                 }else if(grabbedObject.name.Contains("YellowMagic")){
                         GameInventory.Inventory.Add(YellowMagic);
+                        yellowNum++; 
                         temp=YellowMagic;
                          Debug.Log("Logged Yello"); 
                 }else{
                         temp=RedMagic; 
                         GameInventory.Inventory.Add(RedMagic);
-                         Debug.Log("Logged Red"); 
+                        redNum++;
+                        Debug.Log("Logged Red"); 
                 }
 
-                updateText.text = "Update: Collected " +  temp.name + " with val of " + temp.GetComponent<Collectible>().pointVal; 
+                updateText.text = "You Have: Red(10pts): " + redNum + " " +  "Blue(30pts): " + blueNum + " " + "Green(20pts): " + greenNum + " " + "Yellow(50pts): " + yellowNum; 
                 score = score + temp.GetComponent<Collectible>().pointVal; 
                 scoreText.text = "Husam's Score: " + score; 
             }
